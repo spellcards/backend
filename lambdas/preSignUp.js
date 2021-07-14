740e8e42704b785e01424cdd4df2072378efc115
Ben Howard <62419637+ben-howard-55@users.noreply.github.com>
2021-07-14 17:05:53 +1200

    added preSignUp lambda and mysql2

---
+++ lambdas/preSignUp.js
@@ -0,0 +1,36 @@
+const Responses = require('../API_Responses');
+const mysql2 = require('mysql2/promise');
+const AWS = require('aws-sdk');
+
+// db params
+let endpoint = "";
+let username = "";
+let password = "";
+let database_name = "";
+
+
+exports.handler = async event => {
+    console.log('event: ', event);
+
+    // get username
+    let student_name = "Hi";
+
+    mysql
+      .createConnection({
+        host: endpoint,
+        user: username,
+        password: password,
+        database: database_name,
+      })
+      .then((conn) => {
+        const res = conn.query(
+          "INSERT INTO Students (student_name) VALUES (?)",
+          student_name
+        );
+        conn.end();
+        return res;
+      })
+      .then(([rows, fields]) => console.log(rows))
+      .catch((err) => console.log(err));
+};
+
